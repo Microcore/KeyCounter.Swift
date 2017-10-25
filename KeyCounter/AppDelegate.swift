540086cebcfb53b61cadffe824a6f3181bd2fe2c
Joker Qyou <Joker.Qyou@gmail.com>
2017-10-25 20:23:38 +0800

    Initial Commit

---
+++ KeyCounter/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  KeyCounter
+//
+//  Created by Joker Qyou on 2017/10/25.
+//  Copyright © 2017年 Microcore Team. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
